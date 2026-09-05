e0e0750ed3eef922221349056978be540358a1e7
DavidXP5 <david.x.patty@gmail.com>
2026-09-05 15:32:38 -0500

    Add PWA support and Desktop installation

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,16 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(() => {
+        console.log('Service worker registered successfully.')
+      })
+      .catch((error) => {
+        console.error('Service worker registration failed:', error)
+      })
+  })
+}
